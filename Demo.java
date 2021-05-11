df74639b48540b86d3f1a6bb872ec6328a93ea5c
meetsanket24@gmail.com <meetsanket24@gmail.com>
2021-05-11 13:22:43 +0530

    initial commit

---
+++ Demo.java
@@ -0,0 +1,8 @@
+class Demo {
+    public static void main(String args[]) {
+        System.out.println("Table of 2");
+        for(int i=1; i<= 10; i++) {
+            System.out.println("Value of 2 * " + i + " = " + (2*i));
+        }
+    }
+}
