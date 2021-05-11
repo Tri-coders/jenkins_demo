f8c6a5cae5578b17c30c3c555be14587dc559512
meetsanket24@gmail.com <meetsanket24@gmail.com>
2021-05-11 14:08:51 +0530

    table of 2 changes to table of 3

--- Demo.java
+++ Demo.java
@@ -2,7 +2,7 @@ class Demo {
     public static void main(String args[]) {
         System.out.println("Table of 3");
         for(int i=1; i<= 10; i++) {
-            System.out.println("Value of 3 * " + i + " = " + (3*i));
+            System.out.println("Value of [3 * " + i + "] = " + (3*i));
         }
     }
 }
